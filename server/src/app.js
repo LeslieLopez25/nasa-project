eb026b6c51a8ff7914564f7210733290c1d24e75
Leslie Lopez <leslie2515lopez@gmail.com>
2024-04-11 21:07:44 -0600

    Added cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
+cors = require("cors");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost3000",
+  })
+);
 app.use(express.json());
 app.use(planetsRouter);
 
