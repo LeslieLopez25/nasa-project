d5e9850c13b86f4542d1176b61bc8ab14ff895a0
Leslie Lopez <leslie2515lopez@gmail.com>
2024-05-01 19:18:06 -0600

    Connecting POST/launches with front end dashboard

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,9 +1,9 @@
 const API_URL = "http://localhost:8000";
 
 async function httpGetPlanets() {
+  // Load planets and return as JSON.
   const response = await fetch(`${API_URL}/planets`);
   return await response.json();
-  // Load planets and return as JSON.
 }
 
 async function httpGetLaunches() {
@@ -16,8 +16,20 @@ async function httpGetLaunches() {
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
   // Submit given launch data to launch system.
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch (err) {
+    return {
+      ok: false,
+    };
+  }
 }
 
 async function httpAbortLaunch(id) {
