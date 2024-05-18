bc04be80d56957104f9b304ad1778228c9c3965b
Leslie Lopez <leslie2515lopez@gmail.com>
2024-05-18 00:06:00 -0600

    Connecting to mongodb and mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,29 @@
 const http = require("http");
 
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URI = process.env.MONGO_URI;
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URI);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
