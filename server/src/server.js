285c5585b73408e95b072ce13b7353279c165f6a
Leslie Lopez <leslie2515lopez@gmail.com>
2024-05-27 23:25:58 -0600

    Updating tests for Mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,29 +1,15 @@
 const http = require("http");
 
-require("dotenv").config();
-
-const mongoose = require("mongoose");
-
 const app = require("./app");
-
+const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URI = process.env.MONGO_URI;
-
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready!");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URI);
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
