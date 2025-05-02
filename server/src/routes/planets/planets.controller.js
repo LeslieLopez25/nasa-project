5633b50232184733ddaab0a05d348f39cada6298
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-01 22:03:43 -0600

    Added comments to planets controller

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,5 +1,6 @@
 const { getAllPlanets } = require("../../models/planets.model");
 
+// Controller function to handle GET requests for all habitable planets
 async function httpGetAllPlanets(req, res) {
   return res.status(200).json(await getAllPlanets());
 }
