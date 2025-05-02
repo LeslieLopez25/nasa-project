81e8d3755ece7e3c36b442b1a0342448e554ab4d
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-01 22:07:07 -0600

    Added comments to planets router

--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -4,6 +4,7 @@ const planetsRouter = express.Router();
 
 const { httpGetAllPlanets } = require("./planets.controller");
 
+// When someone makes a GET request to this route, it calls the controller function to fetch and return planet data
 planetsRouter.get("/", httpGetAllPlanets);
 
 module.exports = planetsRouter;
